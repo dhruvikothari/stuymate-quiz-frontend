ac30296be2e89d940355ac778b6895a9951e23dc
Dhruvi Kothari <dhruvi.kothari03@gmail.com>
2024-05-03 00:23:29 +0530

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => {
+	return (
+		<main>
+			<h2 className="mt-5">Study Mate - The future of online learning.</h2>
+		</main>
+	)
+}
+
+export default Home
